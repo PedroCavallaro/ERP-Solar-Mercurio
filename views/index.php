66091c81129e529a98f7e2fb7394d56393f0faa4
PedroCavallaro <pedrolemecavallaro@gmail.com>
2023-05-18 12:35:55 -0300

    Frontend layout

---
+++ views/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/index.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div>
+                <img  class="navImg" src="./assets/usericon.png" alt="" srcset="">
+                <p>Usuário 001</p>
+            </div>
+            <div>
+                <img class="navImg" src="./assets/gear.png" alt="gearIcon" srcset="">
+                <p>Configurações</p>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="left-menu">
+            <div class="buttons-container">
+                <input type="button" value="Página Inicial">
+                <input type="button" value="Compromissos">
+                <input type="button" value="Movimentações">
+                <input type="button" value="Fornecedores">
+                <input type="button" value="Bancos">
+            </div>
+            <div class="mercury-main-container">
+                <div class="mercury-container">
+                    <img class="mercury" src="./assets/Mercury.png" alt="teste">
+                </div>
+                <div class="planet-name">
+                    <h1>Mercúrio</h1>
+                </div>
+            </div>
+        </section>
+        <section class="rigth-menu">
+            <div class="info">
+                <h1>Compromissos</h1>
+                <table>
+                    <th>Cód Pedido</th>
+                    <th>Fornecedor</th>
+                    <th>Valor a pagar</th>
+                    <th>Data de Vencimento</th>
+                    <th>Status</th>
+                </table>
+            </div>
+           
+            <div class="info">
+                <h1>Movimentações</h1>
+                <table>
+                    <th>Cód Pedido</th>
+                    <th>Fornecedor</th>
+                    <th>Valor a pagar</th>
+                    <th>Data de Vencimento</th>
+                    <th>Status</th>
+                </table>
+            </div>
+           
+        </section>
+    </main>
+</body>
+<script src="https://use.fontawesome.com/1278951f00.js"></script>
+</html>
